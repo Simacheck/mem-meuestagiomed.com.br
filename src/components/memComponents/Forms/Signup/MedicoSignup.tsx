dcf8f85f499fdb886122517bad7d4699d124d014
Marcelo Borges~ <mfavaro@actioncargo.com.br>
2023-09-10 23:31:35 -0300

    feat: arrumando commit

--- src/components/memComponents/Forms/Signup/MedicoSignup.tsx
+++ src/components/memComponents/Forms/Signup/MedicoSignup.tsx
@@ -25,6 +25,7 @@ const convertToBase64 = (file: any ): Promise<string> => {
   return new Promise((resolve, reject) => {
     const fileReader = new FileReader();
 
+    
     fileReader.readAsDataURL(file);
     fileReader.onload = () => resolve(fileReader.result as string);
     fileReader.onerror = () => reject(new Error("error on tranform to base64"));
